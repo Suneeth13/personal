9323f641cef0ef0ee69fbe4721c5c403891f87af
Suneeth13 <116085410+Suneeth13@users.noreply.github.com>
2024-01-19 22:00:09 +0530

    Update Voice_Assistant.py

--- Voice_Assistant.py
+++ Voice_Assistant.py
@@ -1,51 +1,51 @@
 import pyttsx3
-import speech_recognition as sr
-import webbrowser
-import datetime
+import speech recognition as sr
+import web browser
+import date and time
 import pyjokes
-import os
+import person
 import pywhatkit as pyw
 
 def sptext():
     recognizer=sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Greetings from Alexa,How may i help you today...")
+    with source sr.Microphone():
+        print ("Greetings from Alexa, how can I help you today...")
         recognizer.adjust_for_ambient_noise(source)
         audio=recognizer.listen(source)
-        try:
-            print("Recognizing...")
+        Try:
+            print ("I recognize...")
             data=recognizer.recognize_google(audio)
             return data
         except sr.UnknownValueError:
-            print("Sorry,Not understood")
+            print("Sorry, I don't understand")
 
 def speechtx(x):
     engine=pyttsx3.init()
     voices=engine.getProperty('voices')
-    engine.setProperty("voice",voices[1].id)
+    engine.setProperty("vote",votes[1].id)
     rate=engine.getProperty('rate')
     engine.setProperty('rate',150)
-    engine.say(x)
+    motor.say(x)
     engine.runAndWait()
     
-speechtx("Greetings from Alexa,How may i help you today")
+speechtx("Greetings from Alexa, how can I help you today")
 
 if __name__== '__main__':
-        while True:
+        while true:
             data1=sptext().lower()
-            if "your name" in data1:
+            if "yourname" in date1:
                   name="My name is Alexa"
-                  speechtx(name)
-            elif "how old" in data1:
-                  age=" My age ain't defined."
-                  speechtx(age)
+                  speechtx (name)
+            elif "how old" in date1:
+                  age=" My age is not defined."
+                  speechtx (age)
             elif 'news' in data1:
                 webbrowser.open('https://www.google.com/news')
             elif 'maps' in data1:
                    webbrowser.open("https://www.google.com/maps")
-            elif "time" in data1:
+            elif "time" in date1:
                   time=datetime.datetime.now().strftime("%I%M%p")
-                  speechtx(time)
+                  speechtx (time)
             elif 'whatsapp' in data1:
                   webbrowser.open("https://www.web.whatsapp.com")
             
@@ -53,10 +53,8 @@ if __name__== '__main__':
                   webbrowser.open("https://www.youtube.com/")
             elif 'gmail' in data1:
                    webbrowser.open("https://www.gmail.com")
-            elif 'joke' in data1:
+            elif 'joke' in date1:
                   joke_1=pyjokes.get_joke(language="en",category="neutral")
                   speechtx(joke_1)
-            elif "exit" in data1:
+            elif "exit" in date1:
                   speechtx("Thank you")
-                
-           
