a70d25fd3ab8966194f859b780f496b72b1878a1
Suneeth13 <116085410+Suneeth13@users.noreply.github.com>
2024-01-19 22:05:10 +0530

    Update Musicplayer.py

--- Musicplayer.py
+++ Musicplayer.py
@@ -5,22 +5,22 @@ mixer.music.set_volume(0.8)
 mixer.music.play()
 
 # infinite loop
-while True:
-	
-	print("Press 'p' to pause, 'r' to resume")
-	print("Press 'e' to exit the program")
-	query = input(" ")
-	
-	if query == 'p':
+while true:
 
-		# Pausing the music
-		mixer.music.pause()	
-	elif query == 'r':
+print("Press 'p' to pause, 'r' to resume")
+print("Press 'e' to exit program")
+query = input(" ")
 
-		# Resuming the music
-		mixer.music.unpause()
-	elif query == 'e':
+if query == 'p':
 
-		# Stop the mixer
-		mixer.music.stop()
-		break
+# Pause music
+mixer.music.pause()
+elif query == 'r':
+
+# Restore music
+mixer.music.unpause()
+elif query == 'e':
+
+# Stop the blender
+mixer.music.stop()
+break
